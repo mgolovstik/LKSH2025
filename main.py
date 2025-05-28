ce87fe047662a2b1a62d40f829cc92b18807c789
zog34bro <mgolovstik@gmail.com>
2025-05-28 13:29:44 +0400

    Некоторые функции перенесены в класс Sport_Database. Данные перенесены в папки data и test_data.

--- main.py
+++ main.py
@@ -2,129 +2,137 @@ import requests
 import pandas as pd
 import dotenv
 
-config = dotenv.dotenv_values('.env')
-token = config['token']
-request_url = config['request_url']
-make_requests = (config['make_requests'] == 'True')
-save_info = (config['save_info'] == 'True')
-matches_saving_file = config['matches_saving_file']
-teams_saving_file = config['teams_saving_file']
-players_saving_file = config['players_saving_file']
-
-def make_request_teams():
-    r"""Получает данные о командах с сервера при запуске.
-    
-    Если сервер не отвечает или make_requests=False, то возвращает None"""
-    if not make_requests:
-        return None
-    response = requests.get(request_url + "/teams", headers={"Authorization": token})
-    if not response.ok:
-        return None
-    teams = response.json()
-    teams = pd.DataFrame(teams)
-    return teams
-
-def make_request_players(players_id):
-    r"""Получает данные о игроках с сервера при запуске.
-    
-    Если сервер не отвечает или make_requests=False, то возвращает None"""
-    if not make_requests:
-        return None
-    players = pd.DataFrame({"id": [], "name": [], "surname": [], "number": []})
-    for id in players_id:
-        response = requests.get(request_url + "/players/" + str(id), headers={"Authorization": token})
-        if not response.ok:
-            return None
-        player = response.json()
-        players.loc[len(players)] = player
-    return players
+class Sport_Database():
+    def __make_request_teams(self):
+        r"""Получает данные о командах с сервера при запуске.
         
-def make_request_matches():
-    r"""Получает данные о матчах с сервера при запуске.
+        Если сервер не отвечает или make_requests=False, то возвращает None"""
+        if not self.make_requests:
+            return None
+        response = requests.get(self.request_url + "/teams", headers={"Authorization": self.token})
+        if not response.ok:
+            self.last_failed_request = self.request_url + "/teams"
+            self.response_to_last_failed_request = response
+            return None
+        teams = response.json()
+        teams = pd.DataFrame(teams)
+        return teams
+
+    def __make_request_players(self, players_id):
+        r"""Получает данные о игроках с сервера при запуске.
+        
+        Если сервер не отвечает или make_requests=False, то возвращает None"""
+        if not self.make_requests:
+            return None
+        players = pd.DataFrame({"id": [], "name": [], "surname": [], "number": []})
+        for id in players_id:
+            response = requests.get(self.request_url + "/players/" + str(id), headers={"Authorization": self.token})
+            if not response.ok:
+                self.last_failed_request = self.request_url + "/players" + str(id)
+                self.response_to_last_failed_request = response
+                return None
+            player = response.json()
+            players.loc[len(players)] = player
+        return players
+            
+    def __make_request_matches(self):
+        r"""Получает данные о матчах с сервера при запуске.
+        
+        Если сервер не отвечает или make_requests=False, то возвращает None"""
+        if not self.make_requests:
+            return None
+        response = requests.get(self.request_url + "/matches", headers={"Authorization": self.token})
+        if not response.ok:
+            self.last_failed_request = self.request_url + "/matches"
+            self.response_to_last_failed_request = response
+            return None
+        matches = response.json()
+        matches = pd.DataFrame(matches)
+        return matches
+
+    def __init__(self, config):
+        r"""Инициализирует переменные __teams, __matches, __players.
+        
+        Если сервер не работает или make_requests=False, то берёт сохранённые данные. Если save_info=True, то сохраняет данные
+        """
+
+        self.__token = config['token']
+        self.__request_url = config['request_url']
+        self.make_requests = (config['make_requests'] == 'True')
+        self.save_info = (config['save_info'] == 'True')
+        self.matches_saving_file = config['matches_saving_file']
+        self.teams_saving_file = config['teams_saving_file']
+        self.players_saving_file = config['players_saving_file']
+        self.last_failed_request = None
+        self.response_to_last_failed_request = None
+
+        self.__teams = self.__make_request_teams()
+        if self.__teams is None:
+            self.__teams = pd.read_csv(self.teams_saving_file)
+        elif self.save_info:
+            self.__teams.to_csv(self.teams_saving_file)
+
+        self.__matches = self.__make_request_matches()
+        if self.__matches is None:
+            self.__matches = pd.read_csv(self.matches_saving_file)
+        elif self.save_info:
+            self.__matches.to_csv(self.matches_saving_file)
+
+        players_and_teams_id = pd.DataFrame({'id': [], 'team_id': []})
+        for i in range(len(self.__teams)):
+            lst = self.__teams['players'][i]
+            team_id = self.__teams['id'][i]
+            for x in lst:
+                players_and_teams_id.loc[len(players_and_teams_id)] = {'id': x, 'team_id': team_id}
+        self.__players = self.__make_request_players(players_and_teams_id['id'])
+        if self.__players is None:
+            self.__players = pd.read_csv(self.players_saving_file)
+        else:
+            self.__players = pd.merge(self.__players, players_and_teams_id, on='id')
+            if self.save_info:
+                self.players.to_csv(self.players_saving_file)
+        self.__players['name'] = self.__players['name'].apply(lambda x: '' if pd.isna(x) else x)
+        self.__players['surname'] = self.__players['surname'].apply(lambda x: '' if pd.isna(x) else x)
+
+    def stats(self, team_name):
+        """Возвращает статистику по команде (количество побед, поражений, разница между забитыми и пропущенными голами)"""
+        if len(self.__teams[self.__teams['name'] == team_name]) == 0:
+            return 0, 0, 0
+        team_id = list(self.__teams[self.__teams['name'] == team_name]['id'])[0]
+        wins = len(self.__matches[(self.__matches['team1'] == team_id) & (self.__matches['team1_score'] > self.__matches['team2_score'])])\
+            + len(self.__matches[(self.__matches['team2'] == team_id) & (self.__matches['team1_score'] < self.__matches['team2_score'])])
+        losings = len(self.__matches[(self.__matches['team1'] == team_id) & (self.__matches['team1_score'] < self.__matches['team2_score'])])\
+            + len(self.__matches[(self.__matches['team2'] == team_id) & (self.__matches['team1_score'] > self.__matches['team2_score'])])
+        goals = self.__matches[self.__matches['team1'] == team_id]['team1_score'].sum() + self.__matches[self.__matches['team2'] == team_id]['team2_score'].sum()\
+            - self.__matches[self.__matches['team2'] == team_id]['team1_score'].sum() - self.__matches[self.__matches['team1'] == team_id]['team2_score'].sum()
+        return wins, losings, int(goals)
+
+    def get_team(self, player_id):
+        """Возвращает id команды по id игрока. Если такого игрока нет, то возвращает None"""
+        if len(self.__players[self.__players['id'] == player_id]) != 1:
+            return None
+        return list(self.__players[self.__players['id'] == player_id]['team_id'])[0]
+
+    def versus(self, player1_id, player2_id):
+        team1_id = self.get_team(player1_id)
+        team2_id = self.get_team(player2_id)
+        if team1_id is None or team2_id is None or team1_id == team2_id:
+            return 0
+        return len(self.__matches[((self.__matches['team1'] == team1_id) & (self.__matches['team2'] == team2_id)) |\
+                        ((self.__matches['team1'] == team2_id) & (self.__matches['team2'] == team1_id))])
+        
+    def get_list_of_players(self):
+        """Возвращает список игроков в лексикографическом порядке"""
+        lst = []
+        for i in range(len(self.__players)):
+            lst.append(str(self.__players['name'][i]) + ' ' + str(self.__players['surname'][i]))
+        lst.sort()
+        return lst
     
-    Если сервер не отвечает или make_requests=False, то возвращает None"""
-    if not make_requests:
-        return None
-    response = requests.get(request_url + "/matches", headers={"Authorization": token})
-    if not response.ok:
-        return None
-    matches = response.json()
-    matches = pd.DataFrame(matches)
-    return matches
-
-def init(more_info=False):
-    r"""Возвращает переменные teams, matches, players.
-    
-    Если сервер не работает или make_requests=False, то берёт сохранённые данные. Если save_info=True, то сохраняет данные
-
-    Если more_info=True, то также возвращает получены ли переменные от сервера. (True - данные получены от сервера)
-    """
-    teams = make_request_teams()
-    teams_from_server = True
-    if teams is None:
-        teams_from_server = False
-        teams = pd.read_csv(teams_saving_file)
-    elif save_info:
-        teams.to_csv(teams_saving_file)
-
-    matches = make_request_matches()
-    matches_from_server = True
-    if matches is None:
-        matches_from_server = False
-        matches = pd.read_csv(matches_saving_file)
-    elif save_info:
-        matches.to_csv(matches_saving_file)
-
-    players_and_teams_id = pd.DataFrame({'id': [], 'team_id': []})
-    for i in range(len(teams)):
-        lst = teams['players'][i]
-        team_id = teams['id'][i]
-        for x in lst:
-            players_and_teams_id.loc[len(players_and_teams_id)] = {'id': x, 'team_id': team_id}
-    players = make_request_players(players_and_teams_id['id'])
-    players_from_server = True
-    if players is None:
-        players_from_server = False
-        players = pd.read_csv(players_saving_file)
-    else:
-        players = pd.merge(players, players_and_teams_id, on='id')
-        if save_info:
-            players.to_csv(players_saving_file)
-    players['name'] = players['name'].apply(lambda x: '' if pd.isna(x) else x)
-    players['surname'] = players['surname'].apply(lambda x: '' if pd.isna(x) else x)
-
-    if more_info:
-        return teams, matches, players, teams_from_server, matches_from_server, players_from_server
-    return teams, matches, players
-
-teams, matches, players, teams_from_server, matches_from_server, players_from_server = init(more_info=True)
-
-def stats(team_name):
-    """Возвращает статистику по команде (количество побед, поражений, разница между забитыми и пропущенными голами)"""
-    if len(teams[teams['name'] == team_name]) == 0:
-        return 0, 0, 0
-    team_id = list(teams[teams['name'] == team_name]['id'])[0]
-    wins = len(matches[(matches['team1'] == team_id) & (matches['team1_score'] > matches['team2_score'])])\
-        + len(matches[(matches['team2'] == team_id) & (matches['team1_score'] < matches['team2_score'])])
-    losings = len(matches[(matches['team1'] == team_id) & (matches['team1_score'] < matches['team2_score'])])\
-        + len(matches[(matches['team2'] == team_id) & (matches['team1_score'] > matches['team2_score'])])
-    goals = matches[matches['team1'] == team_id]['team1_score'].sum() + matches[matches['team2'] == team_id]['team2_score'].sum()\
-        - matches[matches['team2'] == team_id]['team1_score'].sum() - matches[matches['team1'] == team_id]['team2_score'].sum()
-    return wins, losings, int(goals)
-
-def get_team(player_id):
-    """Возвращает id команды по id игрока. Если такого игрока нет, то возвращает None"""
-    if len(players[players['id'] == player_id]) != 1:
-        return None
-    return list(players[players['id'] == player_id]['team_id'])[0]
-
-def versus(player1_id, player2_id):
-    team1_id = get_team(player1_id)
-    team2_id = get_team(player2_id)
-    if team1_id is None or team2_id is None or team1_id == team2_id:
-        return 0
-    return len(matches[((matches['team1'] == team1_id) & (matches['team2'] == team2_id)) |\
-                       ((matches['team1'] == team2_id) & (matches['team2'] == team1_id))])
+    def get_data(self):
+        """Возвращает переменные __teams, __matches, __players"""
+        return self.__teams, self.__matches, self.__players
+            
 
 def get_request():
     """Получает запрос от пользователя"""
@@ -140,37 +148,29 @@ def get_request():
         return "versus", [int(request[1][0]), int(request[1][1])]
     else:
         return "?", []
-    
-def get_list_of_players():
-    """Возвращает список игроков в лексикографическом порядке"""
-    lst = []
-    for i in range(len(players)):
-        lst.append(str(players['name'][i]) + ' ' + str(players['surname'][i]))
-    lst.sort()
-    return lst
 
-def process_request(request_type, params):
+def process_request(request_type, params, sdb):
     """Обрабатывает запрос от пользователя"""
     if request_type == "exit":
         return False
     
     if request_type == "stats":
-        wins, losings, goals = stats(params[0])
+        wins, losings, goals = sdb.stats(params[0])
         print(wins, losings, goals)
     elif request_type == "versus":
-        print(versus(params[0], params[1]))
+        print(sdb.versus(params[0], params[1]))
     else:
         print("Unknow request")
     return True
 
 if __name__ == "__main__":
-    all_players = get_list_of_players()
+    config = dotenv.dotenv_values('.env')
+    sdb = Sport_Database(config=config)
+    all_players = sdb.get_list_of_players()
     for x in all_players:
         print(x)
 
     while True:
         request_type, params = get_request()
-        if not process_request(request_type, params):
+        if not process_request(request_type, params, sdb):
             break
-        
-
